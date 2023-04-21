1166be9ae29f4e82b0c577a5fe0be5ff38a54291
desarrollo <desarrollo@academicos.cesde.edu.co>
2023-04-21 11:42:36 -0500

    se crea grupo mascotas

---
+++ happy pet/src/Formulario/Formulario.jsx
@@ -0,0 +1,71 @@
+import './Formulario.css'
+export function Formulario() {
+    return (
+        <>
+
+            <div className="container-fluid formulario">
+                <div className="row justify-content-center">
+                    <div className="col-4">
+                        <form className="">
+                            <div className="row">
+                                <div className="col-12">
+                                    <div class="input-group mb-3">
+                                        <span class="input-group-text" id="basic-addon1">👤</span>
+                                        <input type="text" class="form-control" placeholder="Nombre de usuario" aria-label="Username" aria-describedby="basic-addon1" />
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <div className="row">
+
+                                <div className="col-12">
+                                    <div class="input-group mb-3">
+                                        <span class="input-group-text" id="basic-addon1">📱</span>
+                                        <input type="text" class="form-control" placeholder="Telefono" aria-label="telefono" aria-describedby="basic-addon1" />
+                                    </div>
+                                </div>
+                            </div>
+
+
+
+                            <div className="row">
+                                <div className="col-12">
+                                    <div class="input-group mb-3">
+                                        <span class="input-group-text" id="basic-addon1">💌</span>
+                                        <input type="email" class="form-control" placeholder="email" aria-label="email" aria-describedby="basic-addon1" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-12">
+                                    <div class="input-group mb-3">
+                                        <span class="input-group-text" id="basic-addon1">⌚</span>
+                                        <input type="time" name="hora" value="6:00:00" max="24:00:00" class="form-control" placeholder="Hora" aria-label="Hora" aria-describedby="basic-addon1" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-12">
+                                    <div class="input-group mb-3">
+                                        <span class="input-group-text" id="basic-addon1">🗓</span>
+                                        <input type="date" class="form-control" placeholder="fecha" aria-label="fecha" aria-describedby="basic-addon1" />
+                                    </div>
+                                </div>
+                            </div>
+                            
+                         <button type="submit"className='boton'>Enviar formulario..</button>
+                          
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+        </>
+    )
+}
---
+++ happy pet/src/Home/Home.jsx
@@ -0,0 +1,19 @@
+import { Menu } from '../Shared/Menu/Menu'
+import { Formulario } from '../Formulario/Formulario'
+
+import './Home.css'
+export function Home(){
+    return(
+        <>
+        <Menu/>
+        <section className="banner img-fluid w-80"></section>
+
+        
+        <Formulario/>
+              
+        
+       
+        
+        </>
+    )
+}
